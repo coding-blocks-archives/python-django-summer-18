2bcb68637726b268b097785ae7fbfe609ae23ed5
Jatin Katyal <jatin.katyal13@gmail.com>
2018-07-03 21:43:54 +0530

    Lecture 3 added

---
+++ Lecture 3/code.py
@@ -0,0 +1,5 @@
+import lib as l
+
+p = l.Person()
+
+print(p)
---
+++ Lecture 3/lib.py
@@ -0,0 +1,19 @@
+class Person:
+	pass
+
+
+def add(a, b):
+	return a+b
+
+def sub(a, b):
+	return a-b
+
+def mult(a, b):
+	return a*b
+
+def div(a, b):
+	return a/b
+
+if __name__ == "__main__":
+	print("Hello Welcome to my library")
+print(__name__)
